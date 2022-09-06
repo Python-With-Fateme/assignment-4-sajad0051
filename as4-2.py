30b9c009531d9225d30d7a9ca493a92b19b28e2d
sajjad <sajjadmahmodi051@gmail.com>
2022-09-06 21:09:03 +0430

    assignment4

--- as4-2.py
+++ as4-2.py
@@ -1,14 +1,7 @@
-x=[]
-a=[]
-for i in range(1,1000):
-    n=int(input('enter your numbers?'))
-    if n%2==0:
-        print('true')
-        x.append(n)
-        a.append(n)
-        print('x,a=',(x))
-    if n%2:
-        print('false')
-    if n%2==0:       
-        print('x=',len(x))
-        print('a=',len(a))
+n=int(input('enter yor add?'))
+b=[]
+while len(b)!=n:
+    m=float(input('enter your number? '))
+    if m%2==0:
+        b.append(m)
+    print(b)
--- as4-4.py
+++ as4-4.py
@@ -1,1 +1,23 @@
-
+import random
+from re import T
+filds=['python','c','java','c#','php','c++','javascript']
+fild=random.choice(filds)
+fild=list(fild)
+true_chars=['_']*len(fild)
+print(true_chars)
+scors=len(fild)
+while True:
+    state=False
+    char=input('enter a fild :')
+    for i in range(len(fild)):
+        if fild[i]==char:
+            true_chars[i]=char
+    if state==False:
+        scors-=1
+    print(true_chars)
+    if fild==true_chars:
+        print('you win')
+        break
+    if scors==0:
+        print('you lose')
+        break
